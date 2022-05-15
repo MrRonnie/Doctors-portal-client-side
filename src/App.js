1e4d27f7849ca40aef2e1c0961ccba3c005ceb90
MrRonnie <ronymaza55@gmail.com>
2022-05-15 11:27:54 +0600

    By using Daisy UI created a navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "./Pages/Shared/Navbar";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>This is </h2>
+    </div>
+  );
+};
+
+export default About;
