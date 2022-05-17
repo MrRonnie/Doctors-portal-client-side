f8193075fa3b2477849e2671d33f7bf920b0a6b6
MrRonnie <ronymaza55@gmail.com>
2022-05-17 10:55:39 +0600

    Display a modal from on booking click with dynamic data

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Services from "../Home/Services";
-import PrimaryButton from "../Shared/PrimaryButton";
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
-    <div class="card lg:max-w-lg  bg-base-100 shadow-xl">
+    <div class="card lg:max-w-lg mx-auto bg-base-100 shadow-xl">
       <div class="card-body">
         <h2 class="card-title text-secondary">{name}</h2>
         <p>
@@ -19,12 +17,14 @@ const Service = ({ service }) => {
           {slots.length} {slots.length > 1 ? "spaces" : "space"} available
         </p>
         <div class="card-actions justify-center">
-          <button
+          <label
+            for="booking-modal"
+            onClick={() => setTreatment(service)}
             disabled={slots.length === 0}
-            className="btn btn-secondary  text-white font-bold uppercase border-0"
+            class="btn btn-secondary  text-white font-bold uppercase border-0"
           >
             Book Appointment
-          </button>
+          </label>
         </div>
       </div>
     </div>
