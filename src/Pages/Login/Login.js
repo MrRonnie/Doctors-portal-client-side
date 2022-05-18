b134f79025348e91f624f65eea44ed1337079584
MrRonnie <ronymaza55@gmail.com>
2022-05-18 18:12:52 +0600

    Explore React Hook Form and create email input

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,19 +1,59 @@
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   if (user) {
     console.log(user);
   }
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center text-2xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <label class="label">
+                <span class="label-text-alt">Alt label</span>
+              </label>
+            </div>
+
+            <input {...register("firstName", { required: true })} />
+            {errors.firstName?.type === "required" && "First name is required"}
+
+            <input
+              {...register("lastName", {
+                pattern: {
+                  value: /[A-Za-z]{3}/,
+                  message: "error message",
+                },
+              })}
+            />
+            {errors.lastName && "Last name is required"}
+
+            <input type="submit" />
+          </form>
           <div className="divider">OR</div>
           <button
             onClick={() => signInWithGoogle()}
