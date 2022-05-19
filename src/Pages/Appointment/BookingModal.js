330fb9c7f30d773156a8ad9881c85302ca81604c
MrRonnie <ronymaza55@gmail.com>
2022-05-19 12:55:58 +0600

    Limit one booking per user per treatment per day

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 const BookingModal = ({ date, treatment, setTreatment }) => {
   const { _id, name, slots } = treatment;
   const [user, loading, error] = useAuthState(auth);
-  const formattedDate = format(date, "pp");
+  const formattedDate = format(date, "PP");
 
   const handleBooking = (event) => {
     event.preventDefault();
@@ -32,8 +32,13 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
     })
       .then((res) => res.json())
       .then((data) => {
-        // to close the modal
-        console.log(data);
+        if (data.success) {
+          toast(`Appointment is set, ${formattedDate} at ${slot}`);
+        } else {
+          toast.error(
+            `Already have an Appointment ${data.booking?.date} at ${data.booking?.slot}`
+          );
+        }
         setTreatment(null);
       });
   };
