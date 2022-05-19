32bc15995ec6709003c3529ca99a8840cdd500e8
MrRonnie <ronymaza55@gmail.com>
2022-05-19 17:36:04 +0600

    Display User specific Appointments using Data Table

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,19 +4,13 @@ import { Link, Outlet } from "react-router-dom";
 const Dashboard = () => {
   return (
     <div class="drawer drawer-mobile">
-      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex flex-col items-center justify-center">
-        <h2 className="text-4xl text-base-300">Dashboard</h2>
+      <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <h2 className="text-3xl font-bold text-base-300">Dashboard</h2>
         <Outlet></Outlet>
-        <label
-          for="my-drawer-2"
-          class="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
       </div>
       <div class="drawer-side">
-        <label for="my-drawer-2" class="drawer-overlay"></label>
+        <label for="dashboard-sidebar" class="drawer-overlay"></label>
         <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
           <li>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -30,17 +30,19 @@ const Navbar = () => {
       <li>{user && <Link to="/dashboard">Dashboard</Link>}</li>
       <li>
         {user ? (
-          <button className="btn btn-ghost" onClick={logout}>
+          <button className="btn btn-accent text-white" onClick={logout}>
             Sing Out
           </button>
         ) : (
-          <Link to="/login">Login</Link>
+          <Link className="btn btn-accent text-white" to="/login">
+            Login
+          </Link>
         )}
       </li>
     </>
   );
   return (
-    <div className="navbar lg:justify-evenly sm:justify-around ">
+    <div className="navbar flex-wrap lg:justify-evenly sm:justify-around ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex="0" className="btn btn-ghost lg:hidden">
@@ -72,6 +74,28 @@ const Navbar = () => {
       </div>
       <div className="navbar-center hidden  lg:flex">
         <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <label
+          tabIndex="1"
+          for="dashboard-sidebar"
+          className="btn btn-ghost lg:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
       </div>
     </div>
   );
