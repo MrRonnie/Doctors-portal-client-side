1486a73ab716f9291c68fd0d41023d828e48c706
MrRonnie <ronymaza55@gmail.com>
2022-05-20 15:53:42 +0600

    module 75-3 Issue Simple JWT token and store it in the local storage

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,6 +7,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import useToken from "../../hooks/useToken";
 
 const Login = () => {
   const [signInWithGoogle, googleUser, googleLoading, googleError] =
@@ -20,6 +21,8 @@ const Login = () => {
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
 
+  const [token] = useToken(user || user);
+
   let singInError;
 
   const navigate = useNavigate();
@@ -27,10 +30,10 @@ const Login = () => {
   let from = location.state?.from?.pathname || "/";
 
   useEffect(() => {
-    if (user || googleUser) {
+    if (token) {
       navigate(from, { replace: true });
     }
-  }, [user, googleUser, from, navigate]);
+  }, [token, from, navigate]);
 
   if (loading || googleLoading) {
     return <Loading></Loading>;
