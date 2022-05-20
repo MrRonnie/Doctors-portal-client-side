6fa263142fa553a7c140aa37454d9f058e5e4844
MrRonnie <ronymaza55@gmail.com>
2022-05-20 16:44:22 +0600

    Verify JWT Token and handle unauthorized access

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -14,8 +14,16 @@ const MyAppointment = () => {
           authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
       })
-        .then((res) => res.json())
-        .then((data) => setAppointments(data));
+        .then((res) => {
+          console.log("res", res);
+          if (res.status === 401 || res.status === 403) {
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setAppointments(data);
+        });
     }
   }, [user]);
   return (
