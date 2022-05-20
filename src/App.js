a72aa26dfdf2cf4714013f849dbe3d520e9df3a0
MrRonnie <ronymaza55@gmail.com>
2022-05-20 23:26:39 +0600

    75 module done Prevent accessing Admin route via URL

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MyAppointment from "./Pages/Dashboard/MyAppointment";
 import MyReview from "./Pages/Dashboard/MyReview";
 import MyHistory from "./Pages/Dashboard/MyHistory";
 import Users from "./Pages/Dashboard/Users";
+import RequireAdmin from "./Pages/Login/RequireAdmin";
 
 function App() {
   return (
@@ -41,7 +42,14 @@ function App() {
           <Route index element={<MyAppointment></MyAppointment>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
           <Route path="history" element={<MyHistory></MyHistory>}></Route>
-          <Route path="users" element={<Users></Users>}></Route>
+          <Route
+            path="users"
+            element={
+              <RequireAdmin>
+                <Users></Users>
+              </RequireAdmin>
+            }
+          ></Route>
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
       <li>
         {user ? (
           <button className="btn btn-accent text-white" onClick={logout}>
-            Sing Out
+            Sign Out
           </button>
         ) : (
           <Link className="btn btn-accent text-white" to="/login">
