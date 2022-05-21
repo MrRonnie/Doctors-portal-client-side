dd4bd110bf49ef4003356e57c860f14fbdb8a51d
MrRonnie <ronymaza55@gmail.com>
2022-05-21 17:24:04 +0600

    VerifyAdmin middleware and load all doctors

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -37,6 +37,9 @@ const Dashboard = () => {
               <li>
                 <Link to="/dashboard/addDoctor">Add a Doctor</Link>
               </li>
+              <li>
+                <Link to="/dashboard/manageDoctors">Manage Doctors</Link>
+              </li>
             </>
           )}
         </ul>
